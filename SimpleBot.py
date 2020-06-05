9f916b6b791fdc0135700c9b9af1edbc1a7cdf5c
Lucas Goddin <llgoddin@gmail.com>
2020-06-05 14:49:52 -0400

    Worked out kinks with tile compression alg Added title and tile nums

--- SimpleBot.py
+++ SimpleBot.py
@@ -17,10 +17,16 @@
 import sys
 import pygame
 
+
+LIGHT_GREY = (200, 200, 200)
+GREY = (120, 120, 120)
+DARK_GREY = (40, 40, 40)
+BLACK = (0, 0, 0)
+
 colorDict = {
-    2: (240, 220, 200),
-    4: (255, 234, 199),
-    8: (252, 186, 100),
+    2: (238, 228, 218),
+    4: (210, 180, 170),
+    8: (242, 177, 121),
     16: (245, 149, 99),
     32: (246, 124, 95),
     64: (246, 94, 59),
@@ -29,15 +35,15 @@ colorDict = {
     512: (237, 200, 80),
     1024: (237, 197, 63),
     2048: (237, 194, 46),
-    0: (255, 0, 0)
-    # 0: (238, 228, 218)
+    # 0: (255, 0, 0)
+    0: GREY
 }
 
 # creates a window and sets size and title
 pygame.init()
 pygame.display.set_caption('2048')
-screen = pygame.display.set_mode((600, 800))
-screen.fill((40, 40, 40))
+screen = pygame.display.set_mode((500, 600))
+screen.fill(LIGHT_GREY)
 pygame.display.flip()
 
 
@@ -45,7 +51,7 @@ def gameLoop(board):
     # used to control the main game loop
     userPlaying = True
 
-    printBoard(board)
+    screenUpdate(board)
     while userPlaying:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -59,11 +65,13 @@ def gameLoop(board):
                     simMove(board, 'l')
                 if event.key == pygame.K_RIGHT:
                     simMove(board, 'r')
-                printBoard(board)
+                screenUpdate(board)
+            elif event.type == pygame.KEYUP:
+                print('KEY RELEASE')
 
-        writeBoard(board)
+        # writeBoard(board)
 
-        board = readBoard()
+        # board = readBoard()
 
     sys.exit()
 
@@ -102,6 +110,33 @@ def writeBoard(board):
             boardFile.write('\n')
 
 
+def screenUpdate(board):
+    # draws grid outline
+    pygame.draw.rect(screen, DARK_GREY, (17, 120, 465, 465), 0)
+
+    # draws title font
+    titleText = textRend('2048', 120, DARK_GREY,)
+    textRect = titleText.get_rect()
+    textRect.center = (148, 77)
+    screen.blit(titleText, textRect)
+
+    # draws tiles
+    for i in range(0, 4):
+        for j in range(0, 4):
+
+            # draw the tile
+            pygame.draw.rect(screen, colorDict[board[i][j]], (27 + (j * 115), 130 + (i * 115), 100, 100), 0)
+
+            # create tile text
+            tileText = textRend("" if board[i][j] == 0 else str(board[i][j]), 40, BLACK)
+            tiletextRect = tileText.get_rect()
+            tiletextRect.center = (75 + (j * 115), 180 + (i * 115))
+            screen.blit(tileText, tiletextRect)
+
+    # update the screen
+    pygame.display.flip()
+
+
 def printBoard(board):
     # prints board values to console
     print('-' * 11)
@@ -110,13 +145,11 @@ def printBoard(board):
             print(board[i][j], end=', ')
         print()
 
-    # drawing the board to screen
-    for i in range(0, 4):
-        for j in range(0, 4):
-            pygame.draw.rect(screen, colorDict[board[i][j]], (50 + (j * 133), 50 + (i * 133), 100, 100), 0)
 
-    pygame.display.flip()
-    print('SCREEN UPDATE')
+def textRend(message, size, color):
+    font1 = pygame.font.Font('/System/Library/Fonts/AppleSDGothicNeo.ttc', size)
+    textObj = font1.render(message, True, color)
+    return textObj
 
 
 def combineTiles(board, xDirec, yDirec):
@@ -128,7 +161,7 @@ def combineTiles(board, xDirec, yDirec):
     if not xDirec == 0:
         vert = False
 
-    if xDirec < 0 or yDirec < 0:
+    if xDirec > 0 or yDirec > 0:
         start = 0
         end = 4
         iterator = 1
@@ -169,35 +202,57 @@ def simMove(board, direction):
 
     # determines which direction tiles should be moving and direction the loop should iterate
     if direction == 'l':
-        xDirec = -1
-        iterator = -1
-    elif direction == 'r':
         xDirec = 1
         iterator = 1
-    elif direction == 'u':
-        yDirec = -1
+    elif direction == 'r':
+        xDirec = -1
         iterator = -1
-    else:
+    elif direction == 'u':
         yDirec = 1
         iterator = 1
-
-    if iterator > 0:
-        start = 0
-        end = 3
     else:
+        yDirec = -1
+        iterator = -1
+
+    if iterator < 0:
         start = 3
         end = 0
+    else:
+        start = 0
+        end = 3
 
     # combine tiles
     combineTiles(board, xDirec, yDirec)
+    print('\nPRE COMPRESSION VV')
+    printBoard(board)
 
     # compress tiles
-    for cycles in range(2):
+
+    # The problem with this compression code is simple, its ugly. time to try again
+    # The addition of cycles works but is extremely sloppy, im going to try to clean it up after debugging carefully
+    # I originally thought combination and compression would have to scan the array in opposite directions
+    # The commented code below works for opposite directions but the new code will work for the same direction (FIXED)
+
+    for cycles in range(3):
         for i in range(start, end + xDirec, iterator):
             for j in range(start, end + yDirec, iterator):
-                if board[i + yDirec][j + xDirec] == 0:
-                    board[i + yDirec][j + xDirec] = board[i][j]
-                    board[i][j] = 0
+                if board[i][j] == 0:
+                    board[i][j] = board[i + yDirec][j + xDirec]
+                    board[i + yDirec][j + xDirec] = 0
+
+    # this was a much harder fix than i anticipated, ill try again later
+
+    # for i in range(start, end + xDirec, iterator):
+    #     for j in range(start, end + yDirec, iterator):
+    #         if board[i][j] == 0:
+    #             for searchDist in range(start, end - (j - xDirec)):
+    #                 if board[i + (yDirec * searchDist)][j + (xDirec * searchDist)] != 0:
+    #                     board[i][j] = board[i + (yDirec * searchDist)][j + (xDirec * searchDist)]
+    #                     board[i + (yDirec * searchDist)][j + (xDirec * searchDist)] = 0
+    #                     break
+
+    print('\nPOST COMPRESSION VV')
+    printBoard(board)
 
 
 b = readBoard()
